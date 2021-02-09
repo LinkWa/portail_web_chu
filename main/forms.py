c05bb6281fe1df4891953f71705284ba0ddce512
Antonin Boutet <boutet-23@hotmail.fr>
2021-02-09 22:27:22 +0100

    Ajout bouton suppr + édition + détail et changement de la manière de travailler en passant sur du ModelForm

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,15 @@
-from django import forms
+from django.forms import ModelForm
+
+from main.models import FirstFormModel
 
 
-class FirstForm(forms.Form):
-    firstname = forms.CharField(label="Prénom", max_length=50)
-    lastname = forms.CharField(label="Nom", max_length=50)
-    description = forms.CharField(label="Description", widget=forms.Textarea)
+# class FirstForm(forms.Form):
+#   firstname = forms.CharField(label="Prénom", max_length=50)
+#  lastname = forms.CharField(label="Nom", max_length=50)
+# description = forms.CharField(label="Description", widget=forms.Textarea)
+
+class FirstForm(ModelForm):
+    class Meta:
+        model = FirstFormModel
+        fields = ["first_name", "last_name", "description"]
+        labels = {"first_name": "Prénom", "last_name": "Nom", "description": "Description"}
