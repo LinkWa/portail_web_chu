0c3de834de584948d1414317ddee31288b7d22c4
Samson N <samnzumg@gmail.com>
2021-02-26 13:38:57 +0100

    modification du formulaire

--- main/forms.py
+++ main/forms.py
@@ -6,8 +6,8 @@ from main.models import Recherche, Comment
 class RechercheForm(ModelForm):
     class Meta:
         model = Recherche
-        fields = ["first_name", "last_name", "description"]
-        labels = {"first_name": "Prénom", "last_name": "Nom", "description": "Description"}
+        fields = ["gest_soll", "last_name", "first_name", "proj_name", "proj_acronym", "proj_public_title", "description"]
+        labels = {"gest_soll": "Gestionnaire sollicité : ", "last_name": "Nom de l'investigateur", "first_name": "Prenom de l'investigateur", "proj_name": "Titre complet du projet", "proj_acronym": "Accronyme du projet", "proj_public_title": "Titre grand public du projet", "description": "Description"}
 
 
 class CommentForm(ModelForm):
--- main/models.py
+++ main/models.py
@@ -5,9 +5,30 @@ from django.db import models
 class Recherche(models.Model):
     first_name = models.CharField("Prénom", max_length=50)
     last_name = models.CharField("Nom", max_length=50)
+
+    # description du projet
     description = models.TextField("Description")
     is_valid = models.BooleanField("Validation", default=False)
     linked_id_user = models.PositiveIntegerField(default=0)
+
+    # nom de l'investigateur responsable
+    name_resp = models.CharField(max_length=50)
+
+    # gestionnaire sollicité
+    CHOICES = [
+        ('GEST1', 'CHU Limoges - 2 avenue Martin Luther King, 87042 Limoges Cedex'),
+    ]
+    gest_soll = models.CharField(
+        max_length=5,
+        choices=CHOICES,
+        default='CHU Limoges - 2 avenue Martin Luther King, 87042 Limoges Cedex',
+    )
+    # gest_soll = models.ChoiceField('CHU Limoges - 2 avenue Martin Luther King, 87042 Limoges Cedex')
+
+    # phase1 : initialisation
+    proj_name = models.CharField(max_length=50)  # projet titre complet
+    proj_acronym = models.CharField(max_length=10)
+    proj_public_title = models.CharField(max_length=50)  # projet titre grand public
 
     def __str__(self):
         return self.last_name + " " + self.first_name + " - " + self.description
